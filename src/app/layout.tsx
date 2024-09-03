9244340af26c3536d95778cb0b27de645719fe2e
petercheng <petercheng7788@gmail.com>
2024-09-04 01:36:21 +0800

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="preload" as="image" href="/herobanner.jpg"></link>
       <body className={inter.className}>
         <Header />
         {children}
